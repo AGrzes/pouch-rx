2e34da4682552fe589dedf469ff13e5475037608
Grzegorz Abramczyk <abramczykg@gmail.com>
2025-08-26 20:16:04 +0200

    Refactor override

--- src/override.ts
+++ src/override.ts
@@ -1,7 +1,11 @@
 import debug from 'debug'
+import { MergeFunction } from './merge.js'
 
 const log = debug('ouch-rx:override')
-export const override = (document, { _rev } = {} as any) => {
+export const override: MergeFunction = <T>(
+  document: PouchDB.Core.Document<T>,
+  { _rev }: PouchDB.Core.ExistingDocument<T> = {} as any
+): PouchDB.Core.Document<T> => {
   log('Called with document %o rev %s', document, _rev)
   const result = _rev ? { ...document, _rev } : document
   log('Returning %o', result)
--- test/override.ts
+++ test/override.ts
@@ -5,17 +5,17 @@ import { override } from '../src/override'
 const expect = chai.expect
 describe('override', function () {
   it('should return new document', function () {
-    const doc = { a: 'b' }
+    const doc = { a: 'b', _id: 'id' }
     expect(override(doc)).to.be.deep.equals(doc)
   })
   it('should set _rev based on existing', function () {
-    const doc = { a: 'b' }
-    const existing = { _rev: '_rev' }
-    expect(override(doc, existing)).to.be.deep.equals({ a: 'b', _rev: '_rev' })
+    const doc = { a: 'b', _id: 'id' }
+    const existing = { a: 'c', _rev: '_rev', _id: 'id' }
+    expect(override(doc, existing)).to.be.deep.equals({ a: 'b', _rev: '_rev', _id: 'id' })
   })
   it('should override _rev', function () {
-    const doc = { a: 'b', _rev: '!_rev' }
-    const existing = { _rev: '_rev' }
-    expect(override(doc, existing)).to.be.deep.equals({ a: 'b', _rev: '_rev' })
+    const doc = { a: 'b', _rev: '!_rev', _id: 'id' }
+    const existing = { _rev: '_rev', _id: 'id' }
+    expect(override(doc, existing)).to.be.deep.equals({ a: 'b', _rev: '_rev', _id: 'id' })
   })
 })
