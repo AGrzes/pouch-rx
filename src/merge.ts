4f97f868a434744ee4f7a46a33afff21c7a04910
Grzegorz Abramczyk <abramczykg@gmail.com>
2025-08-26 18:20:06 +0200

    Switched to async/await

--- src/merge.ts
+++ src/merge.ts
@@ -6,24 +6,25 @@ const { isEqual } = lodash
 const log = debug('ouch-rx:merge')
 export const merge = (db, f) => (source) =>
   source.pipe(
-    flatMap((object) => {
+    flatMap(async (object) => {
       log('Saving %o', object)
       const document = f(object)
       log('Transformed %o', document)
-      return db.put(document).catch((error) => {
+      try {
+        return await db.put(document)
+      } catch (error) {
         if (error.name === 'conflict') {
-          return db.get(document._id).then((existing) => {
-            log('Merging %o and %o', object, existing)
-            const merged = f(object, existing)
-            log('Merged %o', merged)
-            const save = merged && !isEqual(merged, existing)
-            log('Save %o', save)
-            return save ? db.put(merged) : Promise.resolve(existing)
-          })
+          const existing = await db.get(document._id)
+          log('Merging %o and %o', object, existing)
+          const merged = f(object, existing)
+          log('Merged %o', merged)
+          const save = merged && !isEqual(merged, existing)
+          log('Save %o', save)
+          return save ? db.put(merged) : existing
         } else {
           log('Error %o', error)
           throw error
         }
-      })
+      }
     })
   )
