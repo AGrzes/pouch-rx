ce5da8faabbf2b2682dce735a2aeb9fa80bfdf94
Grzegorz Abramczyk <abramczykg@gmail.com>
2025-08-26 20:15:52 +0200

    Refactor assign

--- src/assign.ts
+++ src/assign.ts
@@ -1,7 +1,11 @@
 import debug from 'debug'
+import { MergeFunction } from './merge.js'
 
 const log = debug('ouch-rx:assign')
-export const assign = (document, { _rev, ...rest } = {} as any) => {
+export const assign: MergeFunction = <T>(
+  document: PouchDB.Core.Document<T>,
+  { _rev, ...rest }: PouchDB.Core.ExistingDocument<T> = {} as any
+): PouchDB.Core.Document<T> => {
   log('Called with document %o rev %s rest %o', document, _rev, rest)
   const result = _rev ? { ...rest, ...document, _rev } : document
   log('Returning %o', result)
--- test/assign.ts
+++ test/assign.ts
@@ -5,27 +5,27 @@ import { assign } from '../src/assign'
 const expect = chai.expect
 describe('override', function () {
   it('should return new document', function () {
-    const doc = { a: 'b' }
+    const doc = { a: 'b' } as any
     expect(assign(doc)).to.be.deep.equals(doc)
   })
   it('should set _rev based on existing', function () {
-    const doc = { a: 'b' }
-    const existing = { _rev: '_rev' }
+    const doc = { a: 'b' } as any
+    const existing = { _rev: '_rev' } as any
     expect(assign(doc, existing)).to.be.deep.equals({ a: 'b', _rev: '_rev' })
   })
   it('should override _rev', function () {
-    const doc = { a: 'b', _rev: '!_rev' }
-    const existing = { _rev: '_rev' }
+    const doc = { a: 'b', _rev: '!_rev' } as any
+    const existing = { _rev: '_rev' } as any
     expect(assign(doc, existing)).to.be.deep.equals({ a: 'b', _rev: '_rev' })
   })
   it('should copy existing properties', function () {
-    const doc = { a: 'b' }
-    const existing = { _rev: '_rev', c: 'd' }
+    const doc = { a: 'b' } as any
+    const existing = { _rev: '_rev', c: 'd' } as any
     expect(assign(doc, existing)).to.be.deep.equals({ a: 'b', _rev: '_rev', c: 'd' })
   })
   it('should override _rev', function () {
-    const doc = { a: 'b', _rev: '!_rev', c: 'e' }
-    const existing = { _rev: '_rev', c: 'd' }
+    const doc = { a: 'b', _rev: '!_rev', c: 'e' } as any
+    const existing = { _rev: '_rev', c: 'd' } as any
     expect(assign(doc, existing)).to.be.deep.equals({ a: 'b', _rev: '_rev', c: 'e' })
   })
 })
