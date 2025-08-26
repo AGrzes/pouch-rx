41f5f95007c65300be39dbabdc270118dd917754
Grzegorz Abramczyk <abramczykg@gmail.com>
2025-08-26 18:24:44 +0200

    Switched to async/await

--- src/view.ts
+++ src/view.ts
@@ -4,18 +4,17 @@ import { Observable } from 'rxjs'
 const log = debug('ouch-rx:view')
 export const view = (db, name?, options?) => {
   log('Called with name %s and options %o', name, options)
-  return Observable.create((observer) => {
+  return Observable.create(async (observer) => {
     log('Calling query')
-    db.query(name, { ...options, include_docs: true })
-      .then((documents) => {
-        log('Pushing documents')
-        documents.rows.forEach((row) => observer.next(row.doc))
-        log('Finishing')
-        observer.complete()
-      })
-      .catch((err) => {
-        log('Error %o', err)
-        return observer.error(err)
-      })
+    try {
+      const documents = await db.query(name, { ...options, include_docs: true })
+      log('Pushing documents')
+      documents.rows.forEach((row) => observer.next(row.doc))
+      log('Finishing')
+      observer.complete()
+    } catch (err) {
+      log('Error %o', err)
+      return observer.error(err)
+    }
   })
 }
