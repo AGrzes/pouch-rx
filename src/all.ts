fca18ef6488682596b76777dcb783b9725d01ad2
Grzegorz Abramczyk <abramczykg@gmail.com>
2025-08-26 18:26:58 +0200

    Switched to async/await

--- src/all.ts
+++ src/all.ts
@@ -2,20 +2,19 @@ import debug from 'debug'
 import { Observable } from 'rxjs'
 
 const log = debug('ouch-rx:all')
-export const all = (db, options?) => {
+export const all = <T extends {}>(db, options?) => {
   log('Called with options %o', options)
-  return Observable.create((observer) => {
+  return Observable.create(async (observer) => {
     log('Calling allDocs')
-    db.allDocs({ ...options, include_docs: true })
-      .then((documents) => {
-        log('Pushing documents')
-        documents.rows.forEach((row) => observer.next(row.doc))
-        log('Finishing')
-        observer.complete()
-      })
-      .catch((err) => {
-        log('Error %o', err)
-        observer.error(err)
-      })
+    try {
+      const documents = await db.allDocs({ ...options, include_docs: true })
+      log('Pushing documents')
+      documents.rows.forEach((row) => observer.next(row.doc))
+      log('Finishing')
+      observer.complete()
+    } catch (err) {
+      log('Error %o', err)
+      observer.error(err)
+    }
   })
 }
